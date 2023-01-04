7674cb4aa0b2de637998dc87fdb478277786ce74
jakovbasic <jakov.basic@gmail.com>
2023-01-04 18:44:28 +0200

    exercises 3.7 - 3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
 let persons = [
     { 
