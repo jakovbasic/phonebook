5bcd0d732fadc3e2690db8ec522f0799be14724d
jakovbasic <jakov.basic@gmail.com>
2023-01-09 18:41:12 +0200

    exercises 3.15 - 3.18

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if(error.name ==='ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('build'))
+app.use(errorHandler)
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -56,7 +69,7 @@ let persons = [
   app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
       res.json(persons)
-    })
+    }).catch(error => next(error))
   })
 
   app.get('/api/persons/:id', (req, res) => {
@@ -67,12 +80,13 @@ let persons = [
       else {
         res.status(404).end()
       }
-    })
+    }).catch(error => next(error))
   })
 
   app.delete('/api/persons/:id', (req, res) => {
     Person.findByIdAndDelete(req.params.id)
       .then(res.status(204).end())
+        .catch(error => next(error))
   })
 
   const generateId = () => {
@@ -105,6 +119,22 @@ let persons = [
     person.save().then(savedPerson => {
       res.json(savedPerson)
     })
+      .catch(error => next(error))
+  })
+
+  app.put('api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query'  })
+      .then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+        .catch(error => next(error))
   })
 
   const PORT = process.env.PORT || 3001
